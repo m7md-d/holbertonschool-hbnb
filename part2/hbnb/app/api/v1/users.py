0a860153c1ea2e160e2f9d93af34d292f86196f9
JNBLA6 <badrsha321@gmail.com>
2026-02-19 13:45:21 +0300

    feat: enhance user API with registration, retrieval, and update functionalities

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -10,23 +10,22 @@ user_model = api.model('User', {
 })
 
 @api.route('/')
-class Users(Resource):
-    @api.expect(user_model)
-    @api.response(201, 'user sucessfuly register')
-    @api.response(400, 'invalid input')
+class UserList(Resource):
+    @api.expect(user_model, validate=True)
+    @api.response(201, 'User successfully created')
+    @api.response(400, 'Email already registered')
+    @api.response(400, 'Invalid input data')
     def post(self):
-        """register user"""
+        """Register a new user"""
         user_data = api.payload
 
+        # Simulate email uniqueness check (to be replaced by real validation with persistence)
         existing_user = facade.get_user_by_email(user_data['email'])
         if existing_user:
-            return {'error': 'Email already registered'}, 409
+            return {'error': 'Email already registered'}, 400
 
         new_user = facade.create_user(user_data)
-        if new_user:
-            return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
-        else:
-            return {'error': 'Invalid input data'}, 400
+        return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
         
     
     @api.response(200, 'users is fetched')
@@ -35,3 +34,31 @@ class Users(Resource):
         users = facade.get_all_users()
         return [{'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email} for user in users], 200
     
+@api.route('/<user_id>')
+class UserResource(Resource):
+    @api.response(200, 'User details retrieved successfully')
+    @api.response(404, 'User not found')
+    def get(self, user_id):
+        """Get user details by ID"""
+        user = facade.get_user(user_id)
+        if not user:
+            return {'error': 'User not found'}, 404
+        return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+    
+    @api.expect(user_model)
+    @api.response(200, 'User is updated')
+    @api.response(404, 'not found')
+    @api.response(400, 'invild input')
+    def put(self, user_id):
+        """update user"""
+        user_data = api.payload
+
+        user = facade.get_user(user_id)
+        if not user:
+            return {'error': 'User not found'}, 404
+        update_user = facade.update_user(user_id, user_data)
+        if update_user:
+            return{'message': 'User updated successfully'}, 200
+        else:
+            return {'error': 'invalid input'}, 400
+        
